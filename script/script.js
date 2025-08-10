2b88182925cf969aeb4223c6f2a00f0e51520ca1
Palmmy21 <sakdidechneverdie@gmail.com>
2025-08-11 02:16:52 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,8 +4,9 @@ function checkPassword(){
     const input = document.getElementById('password').value;
     const error = document.getElementById('error');
     if(input === passwordFixed){
-        window.location.href = "/src/public/page/thx.html";
+        window.location.href = "/thx.html";
     } else {
         error.textContent = "รหัสไม่ถูกต้อง ลองอีกครั้ง";
     }
 }
+
